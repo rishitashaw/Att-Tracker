dbc1557a74ec21b73cc771edb1dff8d0758a3280
Rishita Shaw <rishitashaw49@gmail.com>
2022-05-06 20:27:02 +0530

    auth added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,18 @@
-import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import Login from './components/login';
 import Profile from './components/profile';
+import {
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1>Att Tracker</h1>
-      <BrowserRouter>
-        <Routes>
-          <Route elements={<Login />} path="/" exact />
-          <Route elements={<Profile />} path="/profile" exact />
-        </Routes>
+      <Routes>
+        <Route element={<Login />} path="/" exact />
+        <Route element={<Profile />} path="/profile" exact />
+      </Routes>
 
-      </BrowserRouter>
     </div>
   );
 }
--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -2,8 +2,7 @@ import "@passageidentity/passage-auth"
 export default function Login() {
     return (
         <>
-            Login
-            <passage-auth app-id="rNZekLj0Q12fg8QrhCeea4uy" />
+            <passage-auth app-id="rNZekLj0Q12fg8QrhCeea4uy" ></passage-auth>
         </>
     )
 
