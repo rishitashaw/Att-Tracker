248819525b036681d1cd40226093d0de375aa353
Purvi2401 <purvi2401binani@gmail.com>
2022-05-14 12:48:05 +0530

    models and serializers done

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,15 +1,42 @@
-from django.db.models import fields
 from rest_framework import serializers
-from .models import SubjectCode, Student
+from .models import *
+
 
 class StudentSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Student
-		fields = ('name', 'roll_number', 'username')
+    class Meta:
+        model = Student
+        fields = ("name", "roll", "email")
 
-class SubjectCodeSerializer(serializers.ModelSerializer):
-	StudentRoot=StudentSerializer(many=True)
-	class Meta:
-		model = SubjectCode
-		fields = ('subject_code', 'created_at')
 
+class LectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lect
+        fields = ("code", "end_time")
+
+    def create(self, data):
+        lect = Lect(
+            code=data["code"],
+            end_time=data["end_time"],
+        )
+        lect.save()
+        print(lect)
+        return lect
+
+
+class Code_StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Code_Student
+        fields = (
+            "email",
+            "code",
+            "end_time",
+        )
+
+    def create(self, data):
+        lecture = Code_Student(
+            email=data["email"],
+            code=data["code"],
+            end_time=data["end_time"],
+        )
+        lecture.save()
+        return lecture
